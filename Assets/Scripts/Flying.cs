acda7e38d41dcd9c03d2103ebf0736ff8a06e18b
Joye Nettles <joyejnettles@gmail.com>
2017-02-01 15:43:38 -0500

    Started superman. Can fly

---
+++ Assets/Scripts/Flying.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour {
+
+	public Transform head;
+
+	public SteamVR_TrackedObject leftHand;
+	public SteamVR_TrackedObject rightHand;
+
+	private bool isFlying = false;
+
+	// Update is called once per frame
+	void Update () {
+		var leftDevice = SteamVR_Controller.Input ((int)leftHand.index);
+		var rightDevice = SteamVR_Controller.Input ((int)rightHand.index);
+		if (leftDevice.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger) || rightDevice.GetTouchDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			isFlying = !isFlying;
+		}
+
+		if (isFlying) {
+			//Calculate distance between head and hands to get direction vector
+			Vector3 leftDir = leftHand.transform.position - head.position;
+			Vector3 rightDir = rightHand.transform.position - head.position;
+
+			Vector3 dir = leftDir + rightDir;
+
+			transform.position += (dir/10); // makes our direction change based on direction
+		}
+	}
+}
